208faaa2623fc1d2175719a7464b19ef07543d20
= <fuxx222.anmeldung@gmail.com>
2024-07-09 09:26:15 +0200

    Add frontend contact system

--- portfolio/index.js
+++ portfolio/index.js
@@ -3,9 +3,35 @@ document.getElementById("cv").onclick = function(){
 }
 
 document.getElementById("contact-submit").onclick = function(){
-    const name = document.getElementById("contact-name").value;
-    const email = document.getElementById("contact-email").value;
-    const message = document.getElementById("contact-message").value;
+    const name = document.getElementById("contact-name");
+    const email = document.getElementById("contact-email");
+    const message = document.getElementById("contact-message");
 
-    console.log(name, email, message);
+    let isEmailValid = email.value.includes("@") && email.value.includes(".");
+
+    if(name.value === "" || email.value === "" || message.value === "" || !isEmailValid){
+        alert("Message not sent! Check if all fields are filled and if the email is valid.");
+        return;
+    }
+
+    // Contact API
+
+    fetch("http://localhost:3000/contact", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            name: name.value,
+            email: email.value,
+            message: message.value
+        })
+    });
+
+    alert("Message sent successfully!");
+
+    name.value = "";
+    email.value = "";
+    message.value = "";
+
 }
