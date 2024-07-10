cecd7a5a3c741861e0877364212cc125e38a7b8b
= <fuxx222.anmeldung@gmail.com>
2024-07-10 15:50:08 +0200

    Update Password Generator

--- password-generator/index.js
+++ password-generator/index.js
@@ -1,7 +1,5 @@
 document.getElementById("generate").onclick = function(){
     const result = document.getElementById("result");
-    const announce = document.getElementById("announce");
-    const copy = document.getElementById("copy-button");
 
     var len = 8;
     var password = "";
@@ -11,18 +9,14 @@ document.getElementById("generate").onclick = function(){
         password += charset.charAt(Math.floor(Math.random() * n));
     }
 
-    announce.style.display = "block";
-    copy.style.display = "block";
-
     result.innerHTML = password;
-}
 
-document.getElementById("copy-button").onclick = function(){
-    const password = document.getElementById("result").innerText;
-    navigator.clipboard.writeText(password).then(function(){
-        console.log("Copied password");
-    }).catch(function(err) {
-        console.error("Could not copy password: ", err);
-        alert("Could not copy password. Check console.");
-    });
+    result.classList.remove("result-before");
+    result.classList.add("result-after");
+
+    confetti({
+        particleCount: 100,
+        spread: 70,
+        origin: { y: 1 },
+      });
 }
