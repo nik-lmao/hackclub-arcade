55fb317a398520566dc5ac2a4de8b21d8b503e0b
= <fuxx222.anmeldung@gmail.com>
2024-07-10 16:06:25 +0200

    Add password validation

--- password-generator/index.js
+++ password-generator/index.js
@@ -1,3 +1,32 @@
+function validatePassword(password){
+    var hasSpecialChar = /[!._]/.test(password);
+    var hasNumber = /\d/.test(password);
+
+    if (!hasSpecialChar) {
+        randomSpecialChar = "!._".charAt(Math.floor(Math.random() * 3));
+        
+        let passwordArray = password.split("");
+        
+        let randomIndex = Math.floor(Math.random() * passwordArray.length);
+        passwordArray[randomIndex] = randomSpecialChar;
+    
+        password = passwordArray.join("");
+    }
+
+    if (!hasNumber) {
+        randomNumber = Math.floor(Math.random() * 10);
+        
+        let passwordArray = password.split("");
+        
+        let randomIndex = Math.floor(Math.random() * passwordArray.length);
+        passwordArray[randomIndex] = randomNumber;
+    
+        password = passwordArray.join("");
+    }
+
+    return password;
+}
+
 document.getElementById("generate").onclick = function(){
     const result = document.getElementById("result");
 
@@ -8,6 +37,8 @@ document.getElementById("generate").onclick = function(){
     for(var i = 0, n = charset.length; i < len; i+=1) {
         password += charset.charAt(Math.floor(Math.random() * n));
     }
+
+    password = validatePassword(password);
 
     result.innerHTML = password;
 
