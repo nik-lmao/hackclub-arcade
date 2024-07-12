b4a799d82387e904505139e1c8a8f726b4a274cb
= <fuxx222.anmeldung@gmail.com>
2024-07-12 20:50:54 +0200

    Add functionality to to-do app

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,7 +1,65 @@
-function addTask(){
-    alert("Implement addTask function");
+function doConfetti() {
+    confetti({
+        particleCount: 100,
+        spread: 70,
+        origin: { y: 1 },
+      });
 }
 
-function deleteTask(task){
-    alert("Implement deleteTask function");
+window.onload = function() {
+    updateTaskCount();
+};
+
+function addTask() {
+    var task = document.getElementById("task").value;
+    
+    if (task === "") {
+        alert("Task cannot be empty");
+        return;
+    }
+
+    var ul = document.getElementById("tasks");
+
+    var div = document.createElement("div");
+    div.className = "task";
+    div.ondblclick = function() {
+        markTask(div);
+    };
+
+    var li = document.createElement("li");
+    li.textContent = task;
+
+    var button = document.createElement("button");
+    button.textContent = "×";
+    button.onclick = function() {
+        deleteTask(div);
+    };
+
+    ul.appendChild(div);
+    div.appendChild(li);
+    div.appendChild(button);
+
+    document.getElementById("task").value = "";
+
+    updateTaskCount();
+
+    doConfetti();
+}
+
+function deleteTask(task) {
+    task.remove();
+}
+
+function clearTasks() {
+    alert("Implement later");
+}
+
+function markTask(task) {
+    
+    alert("Implement later");
+}
+
+function updateTaskCount() {
+    var count = document.getElementById("tasks").childElementCount;
+    document.getElementById("task-count").textContent = `${count} tasks`;
 }
