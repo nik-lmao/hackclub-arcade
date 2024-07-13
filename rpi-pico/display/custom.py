db74c9b5f9bab5bcc972725a06a9cba61c9cb68b
= <fuxx222.anmeldung@gmail.com>
2024-07-13 20:23:52 +0200

    Implement functionality

--- rpi-pico/display/custom.py
+++ rpi-pico/display/custom.py
@@ -16,6 +16,15 @@ lcd = I2cLcd(i2c, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)
 button = Pin(14, Pin.IN, Pin.PULL_UP)
     
 #custom
+
+def updateTime(line):
+    current_time = time.localtime()
+    
+    lcd.move_to(2,line)
+
+    string = "{:02}.{:02} - {:02}:{:02}".format(current_time[2], current_time[1], current_time[3], current_time[4])
+
+    lcd.putstr(string)
     
 lcd.clear()
 
@@ -26,12 +35,48 @@ lcd.putstr("gh @nik-lmao")
 
 time.sleep(2)
 
+lcd.clear()
+
+row = 0
+
+lcd.custom_char(0, bytearray([
+    0x00,
+    0x04,
+    0x0E,
+    0x15,
+    0x04,
+    0x04,
+    0x04,
+    0x00]))
+
+lcd.custom_char(1, bytearray([
+    0x00,
+    0x04,
+    0x04,
+    0x04,
+    0x15,
+    0x0E,
+    0x04,
+    0x00]))
+
+lcd.move_to(0,0)
+lcd.putchar(chr(0))
+lcd.move_to(0,1)
+lcd.putchar(chr(1))
+
+
 while True:
-    lcd.clear()
-    lcd.move_to(0,0)
-    
     if button.value() == 0:
-        lcd.putstr("Pressed")
+        row += 1
+        
+        if row > 10:
+            updateTime(0)
+            row = 0
+            while button.value() == 0:
+                time.sleep(0.1)
     else:
-        lcd.putstr("Not pressed")
-    time.sleep(0.5)
+        if 0 < row <= 10:
+            updateTime(1)
+        row = 0
+    
+    time.sleep(0.1)
