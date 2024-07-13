9d3175075d46d3041129e6ba03f2747906e19dc2
= <fuxx222.anmeldung@gmail.com>
2024-07-13 19:53:04 +0200

    Add button to system

--- rpi-pico/display/custom.py
+++ rpi-pico/display/custom.py
@@ -1,7 +1,8 @@
 import utime
+import time
 
 import machine
-from machine import I2C
+from machine import I2C, Pin
 from lcd_api import LcdApi
 from pico_i2c_lcd import I2cLcd
 
@@ -10,7 +11,9 @@ I2C_NUM_ROWS = 4
 I2C_NUM_COLS = 20
 
 i2c = I2C(0, sda=machine.Pin(0), scl=machine.Pin(1), freq=400000)
-lcd = I2cLcd(i2c, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)  
+lcd = I2cLcd(i2c, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)
+
+button = Pin(14, Pin.IN, Pin.PULL_UP)
     
 #custom
     
@@ -20,3 +23,15 @@ lcd.move_to(1,0)
 lcd.putstr("Nikita Nikitin")
 lcd.move_to(2,1)
 lcd.putstr("gh @nik-lmao")
+
+time.sleep(2)
+
+while True:
+    lcd.clear()
+    lcd.move_to(0,0)
+    
+    if button.value() == 0:
+        lcd.putstr("Pressed")
+    else:
+        lcd.putstr("Not pressed")
+    time.sleep(0.5)
