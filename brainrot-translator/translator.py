73afd8211f770cd1a76e8422e7c11ac515f74181
= <fuxx222.anmeldung@gmail.com>
2024-07-21 10:44:28 +0200

    Update brainrot translate

--- brainrot-translator/translator.py
+++ brainrot-translator/translator.py
@@ -1,4 +1,5 @@
 import random
+import os
 
 def load_words(filename):
     with open(filename, 'r') as file:
@@ -9,6 +10,10 @@ nouns = load_words('nouns.txt')
 adjectives = load_words('adjectives.txt')
 verbs = load_words('verbs.txt')
 
+brainrotNouns = load_words("nouns-brainrot.txt")
+brainrotAdjectives = load_words("adjectives-brainrot.txt")
+brainrotVerbs = load_words("verbs-brainrot.txt")
+
 def brainrot(message):
     words = message.split()
     brainrotted_message = []
@@ -17,15 +22,15 @@ def brainrot(message):
         word_lower = word.lower()
         
         if word_lower in nouns:
-            brainrotted_message.append("sigma")
+            brainrotted_message.append(random.choice(list(brainrotNouns)))
 
         
         elif word_lower in adjectives:
-            brainrotted_message.append("rizzed")
+            brainrotted_message.append(random.choice(list(brainrotAdjectives)))
 
         
         elif word_lower in verbs:
-            brainrotted_message.append("rizz")
+            brainrotted_message.append(random.choice(list(brainrotVerbs)))
 
         else:
             brainrotted_message.append(word)
@@ -33,5 +38,6 @@ def brainrot(message):
     return ' '.join(brainrotted_message)
 
 message = input("Enter a message: ")
+os.system('cls' if os.name == 'nt' else 'clear')
 brainrotted_message = brainrot(message)
 print(brainrotted_message)
