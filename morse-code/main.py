2b7f8db4457b8933d9caf4667fc428d55010d886
= <fuxx222.anmeldung@gmail.com>
2024-07-25 19:04:27 +0200

    Update main.py

--- morse-code/main.py
+++ morse-code/main.py
@@ -3,16 +3,14 @@ import os
 import time
 
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-morse_code_translation = [
-    ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."
-]
+morse_code_translation = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."]
 
-def morse_code_translator(text):
+def string_to_morse(text):
     text = text.upper()
     morse_code = ""
     for letter in text:
         if letter == " ":
-            morse_code += " "
+            morse_code += " // "
         else:
             morse_code += morse_code_translation[alphabet.index(letter)] + " "
     return morse_code
@@ -21,8 +19,8 @@ def main():
     os.system("cls" if os.name == "nt" else "clear")
     print("Enter 'exit' to quit the program")
 
-    random_string = ''.join(random.choices(alphabet, k=5))
-    morse_code = morse_code_translator(random_string)
+    random_string = ' '.join(random.choices(alphabet, k=5))
+    morse_code = string_to_morse(random_string)
 
     guess = input(f"{morse_code}\n-> ")
     if guess == "exit":
@@ -32,10 +30,11 @@ def main():
         print("Correct!")
     else:
         print(f"Incorrect! The correct answer was {random_string}")
+    
     input("Press enter to continue...")
-    time.sleep(2)
+
+print("Welcome to the Morse Code Game! You will be shown a random string of 5 letters in Morse Code. Try to guess the string of letters that the Morse Code represents. Good luck!")
 
 while True:
-    print("Welcome to the Morse Code Game!")
     time.sleep(1)
     main()
