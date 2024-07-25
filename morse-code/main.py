a2c28aeb85a51411d84e492f76e56b3b516110ec
= <fuxx222.anmeldung@gmail.com>
2024-07-25 14:16:49 +0200

    Update main.py

--- morse-code/main.py
+++ morse-code/main.py
@@ -1,4 +1,6 @@
 import random
+import os
+import time
 
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 morse_code_translation = [
@@ -15,13 +17,14 @@ def morse_code_translator(text):
             morse_code += morse_code_translation[alphabet.index(letter)] + " "
     return morse_code
 
-
-
 def main():
+    os.system("cls" if os.name == "nt" else "clear")
+    print("Enter 'exit' to quit the program")
+
     random_string = ''.join(random.choices(alphabet, k=5))
     morse_code = morse_code_translator(random_string)
 
-    guess = input(f"Enter the translation for {morse_code}: ")
+    guess = input(f"{morse_code}\n-> ")
     if guess == "exit":
         print("Goodbye!")
         return
@@ -29,6 +32,10 @@ def main():
         print("Correct!")
     else:
         print(f"Incorrect! The correct answer was {random_string}")
+    input("Press enter to continue...")
+    time.sleep(2)
 
 while True:
+    print("Welcome to the Morse Code Game!")
+    time.sleep(1)
     main()
