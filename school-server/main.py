4fde69b72fb3149f702b8a00adda7c958c605663
= <fuxx222.anmeldung@gmail.com>
2024-07-26 13:48:31 +0200

    Upload session 3

--- school-server/main.py
+++ school-server/main.py
@@ -1,46 +1,45 @@
 import discord
 from discord.ext import commands
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
-
-async def updateMessages():
-    category_id = 1266341510345916416
-    category = bot.get_channel(category_id)
-    if not isinstance(category, discord.CategoryChannel):
-        print(f"Channel with ID {category_id} is not a category.")
-        return
-    
-    channels = category.channels
-
-    for channel in channels:
-        messages = []
-        async for message in channel.history(limit=None):
-            messages.append(message)
-        if len(messages) > 10:
-            index = -1
-            while True:
-                if messages[index].pinned:
-                    index -= 1
-                else:
-                    break
-            await messages[index].delete()
 
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user.name}")
 
+
 @bot.event
-async def on_message(message):
-    await updateMessages()
-    await bot.process_commands(message)
+async def on_member_join(member):
+    off_topic = member.guild.get_channel(1266357527205908572)
+    
+    await off_topic.send(f"👋 | Willkommen auf dem Server, {member.mention}. Registriere dich bitte mit `!register <vorname>`")
+
+@bot.event
+async def on_member_remove(member):
+    off_topic = member.guild.get_channel(1266357527205908572)
+    
+    await off_topic.send(f"{member.name} hat uns verlassen.")
 
 @bot.command()
-async def delete_oldest_message(ctx):
-    channel = ctx.channel
-    messages = []
-    async for message in channel.history(limit=None):
-        messages.append(message)
-    if messages:
-        await messages[-1].delete()
+async def register(ctx, name):
+    role = discord.utils.get(ctx.guild.roles, name="10cT")
 
-bot.run("")
+    nameLower = name.lower()
+
+    with open("./names.txt", "r") as file:
+        names = file.read().split("\n")
+    
+    if nameLower in names:
+        await ctx.message.delete()
+        await ctx.author.add_roles(role)
+        await ctx.send(f"✅ | {ctx.author.mention} wurde erfolgreich registriert.")
+    else:
+        await ctx.send("❗️ | Name nicht gefunden. Bitte gib deinen Vornamen ein.")
+
+
+bot.run(os.getenv("DISCORD_TOKEN"))
