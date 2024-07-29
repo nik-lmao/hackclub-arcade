907ac82b374fb3578cc8689c4b0cf250bc37240d
nik <98088130+nik-lmao@users.noreply.github.com>
2024-07-29 19:25:32 +0200

    Upload calculator app

---
+++ calculator/calculator.py
@@ -0,0 +1,29 @@
+
+print("Simple Calculator")
+print("Select operation:")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+
+choice = input("Enter choice(1/2/3/4): ")
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+if choice == "1":
+    result = num1 + num2
+    print(f"Result: {result}")
+elif choice == "2":
+    result = num1 - num2
+    print(f"Result: {result}")
+elif choice == "3":
+    result = num1 * num2
+    print(f"Result: {result}")
+elif choice == "4":
+    if num2 != 0:
+        result = num1 / num2
+        print(f"Result: {result}")
+    else:
+        print("Cannot divide by zero")
+else:
+    print("Invalid input.")
