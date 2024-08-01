203445c4ec7072da8f99fb23ecf1950e79341de2
nik <98088130+nik-lmao@users.noreply.github.com>
2024-08-01 23:09:22 +0200

    Update website

---
+++ personal-website/index.js
@@ -0,0 +1,7 @@
+document.getElementById("socials").onclick = function(){
+    alert("Socials Button clicked. Functionality will get added later!")
+  }
+  
+  document.getElementById("more").onclick = function(){
+    alert("More Button clicked. Functionality will get added later!")
+  }
