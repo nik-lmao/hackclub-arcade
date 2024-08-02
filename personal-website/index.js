0f95c673ba24f88400e3ec71b760a9439050a086
nik <98088130+nik-lmao@users.noreply.github.com>
2024-08-02 21:25:46 +0200

    add extra infos

--- personal-website/index.js
+++ personal-website/index.js
@@ -1,9 +1,24 @@
 document.getElementById("more").onclick = function() {
   document.getElementById("popup").style.display = "flex";
+  const interestsContainer = document.getElementById("interests");
+  interestsContainer.innerHTML = '';
+  interests.forEach(interest => {
+    const interestDiv = document.createElement("div");
+    interestDiv.className = "interest";
+    interestDiv.innerText = interest;
+    interestsContainer.appendChild(interestDiv);
+  });
 }
 
 document.querySelector(".close-btn").onclick = function() {
   document.getElementById("popup").style.display = "none";
 }
 
-
+const interests = [
+  "photography",
+  "programming",
+  "meeting friends",
+  "biking",
+  "videogames",
+  "listening to music"
+]
