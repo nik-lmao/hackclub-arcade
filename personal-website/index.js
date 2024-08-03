275e4e95611315402a89a08f918a19265db9e931
nik <98088130+nik-lmao@users.noreply.github.com>
2024-08-03 16:14:19 +0200

    Final touches

--- personal-website/index.js
+++ personal-website/index.js
@@ -1,24 +1,30 @@
-document.getElementById("more").onclick = function() {
-  document.getElementById("popup").style.display = "flex";
-  const interestsContainer = document.getElementById("interests");
-  interestsContainer.innerHTML = '';
-  interests.forEach(interest => {
-    const interestDiv = document.createElement("div");
-    interestDiv.className = "interest";
-    interestDiv.innerText = interest;
-    interestsContainer.appendChild(interestDiv);
-  });
+document.getElementById("more").onclick = function() 
+{
+    const popup = document.getElementById("popup");
+    popup.classList.add("show");
+
+    const interestsContainer = document.getElementById("interests");
+    interestsContainer.innerHTML = "";
+    interests.forEach(interest => 
+    {
+        const interestDiv = document.createElement("div");
+        interestDiv.className = "interest";
+        interestDiv.innerText = interest;
+        interestsContainer.appendChild(interestDiv);
+    });
 }
 
-document.querySelector(".close-btn").onclick = function() {
-  document.getElementById("popup").style.display = "none";
+document.querySelector(".close-btn").onclick = function() 
+{
+    const popup = document.getElementById("popup");
+    popup.classList.remove("show");
 }
 
 const interests = [
-  "photography",
-  "programming",
-  "meeting friends",
-  "biking",
-  "videogames",
-  "listening to music"
+    "photography",
+    "programming",
+    "meeting friends",
+    "biking",
+    "videogames",
+    "listening to music"
 ]
