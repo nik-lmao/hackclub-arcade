4b3f0ad7ae67545dbeba9c469c5444df203f0de8
nik <98088130+nik-lmao@users.noreply.github.com>
2024-08-06 17:04:50 +0300

    Update photography gallery

--- photography-gallery/index.js
+++ photography-gallery/index.js
@@ -1,13 +1,56 @@
-const galleryItems = document.querySelectorAll(".gallery-item img");
+const gallery = document.getElementById("gallery");
+
+// URL/photo?
+const choice = 0;
+
+const galleryItems = [
+    "https://cdn.discordapp.com/attachments/1108789026992959610/1268673430215983145/IMG_2445.jpeg?ex=66b33674&is=66b1e4f4&hm=00b57351e333042c8de981e204ba0329ed2b573d3b72c40b173f9dd1b5b53d04&",
+    "https://cdn.discordapp.com/attachments/1108789026992959610/1268673430215983145/IMG_2445.jpeg?ex=66b33674&is=66b1e4f4&hm=00b57351e333042c8de981e204ba0329ed2b573d3b72c40b173f9dd1b5b53d04&",
+    "https://cdn.discordapp.com/attachments/1268677485617483858/1270375944749580360/IMG_2625.jpeg?ex=66b3794b&is=66b227cb&hm=cbd6b82358dfd0c2a485772e320aa9e728f23267146e6bfe12a8cb2658e286c8&",
+    "https://cdn.discordapp.com/attachments/1268677485617483858/1270375944749580360/IMG_2625.jpeg?ex=66b3794b&is=66b227cb&hm=cbd6b82358dfd0c2a485772e320aa9e728f23267146e6bfe12a8cb2658e286c8&",
+    "https://cdn.discordapp.com/attachments/1268677485617483858/1270375944749580360/IMG_2625.jpeg?ex=66b3794b&is=66b227cb&hm=cbd6b82358dfd0c2a485772e320aa9e728f23267146e6bfe12a8cb2658e286c8&",
+    "https://cdn.discordapp.com/attachments/1268677485617483858/1270375944749580360/IMG_2625.jpeg?ex=66b3794b&is=66b227cb&hm=cbd6b82358dfd0c2a485772e320aa9e728f23267146e6bfe12a8cb2658e286c8&"
+];
+
+galleryNames = [
+    "photo1.jpg",
+    "photo2.jpg"
+]
+
+
+window.onload = function(){
+    if(choice == 0){
+        galleryItems.forEach(url => {
+            const galleryItem = document.createElement("div");
+            galleryItem.className = "gallery-item";
+            const img = document.createElement("img");
+            img.src = url;
+            galleryItem.appendChild(img);
+            gallery.appendChild(galleryItem);
+        });
+    }
+    if(choice == 1){
+        galleryNames.forEach(name => {
+            const galleryItem = document.createElement("div");
+            galleryItem.className = "gallery-item";
+            const img = document.createElement("img");
+            img.src = `./photos/${name}`;
+            galleryItem.appendChild(img);
+            gallery.appendChild(galleryItem);
+            
+        })
+    }
+}
+
 const lightbox = document.getElementById("lightbox");
 const lightboxImg = document.getElementById("lightbox-img");
 const closeBtn = document.querySelector(".close");
 
-galleryItems.forEach(item => {
-    item.addEventListener("click", () => {
+gallery.addEventListener("click", (e) => {
+    if (e.target.tagName === "IMG") {
         lightbox.style.display = "block";
-        lightboxImg.src = item.src;
-    });
+        lightboxImg.src = e.target.src;
+    }
 });
 
 closeBtn.addEventListener("click", () => {
